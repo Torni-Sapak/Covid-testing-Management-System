ee8f9fd079e8ae6e2f7fab497662f0028ffded99
Torni-Sapak <51757527+Torni-Sapak@users.noreply.github.com>
2022-01-18 15:59:24 +0000

    Initial commit

---
+++ Covid.session.sql
@@ -0,0 +1,16 @@
+select * from tblpatients
+
+  
+ /*delete  from tblreporttracking
+ where id = 39
+
+ 1.tbladmin
+ 2.tblpatients
+ 3. tblphlebotomist
+ 4.tblreporttracking
+ tbletestrecord
+
+select * from tblreporttracking
+
+ */
+
